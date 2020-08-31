ddf36a448ced4c8778e6cae27e2c241bc13aba56
site-db <oozeooze@naver.com>
2020-08-31 13:59:48 +0900

    # all

---
+++ src/main/java/com/drtoursite/drtourdemo/DrtourdemoApplication.java
@@ -0,0 +1,13 @@
+package com.drtoursite.drtourdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrtourdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrtourdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/drtoursite/drtourdemo/DrtourdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drtoursite.drtourdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrtourdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
